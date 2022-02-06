e4a4e9375eea3af56cd8116131fefbc03f1c154b
Alexey <algoritm211@gmail.com>
2022-02-06 22:02:28 +0200

    Add bubble sort

---
+++ sortingAlgorithms/bubbleSort/bubbleSort.ts
@@ -0,0 +1,17 @@
+
+const bubbleSort = (initialArray: Array<number>) => {
+  // Making copy of existing array. No mutation
+  const array = initialArray.slice();
+
+  for (let n = 0; n < array.length - 1; n++) {
+    for (let i = 0; i < array.length - 1 - n; i++) {
+      if (array[i] > array[i + 1]) {
+        const buff = array[i];
+        array[i] = array[i + 1]
+        array[i + 1] = buff;
+      }
+    }
+  }
+
+  return array;
+}
