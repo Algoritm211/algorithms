0cb5db5def9fda48f7e56301ad34a04607cac76b
Alexey <algoritm211@gmail.com>
2022-02-09 23:14:43 +0200

    Add insertion sort

---
+++ sortingAlgorithms/insertionSort/insertionSort.ts
@@ -0,0 +1,23 @@
+
+const arr = [1, 5, 10, 14, -6, -12, -3, 19];
+
+const insertionSort = (initialArray: Array<number>) => {
+  const array = [...initialArray];
+
+  for (let i = 0; i < array.length; i++) {
+    let n = i;
+    let temp = 0;
+
+    while (n > 0 && array[n] < array[n - 1]) {
+      temp = array[n]
+      array[n] = array[n - 1]
+      array[n - 1] = temp;
+      n -= 1;
+    }
+
+  }
+
+  return array;
+}
+
+console.log(insertionSort(arr));
