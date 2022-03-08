bbcdd6bee5fa649bc683f95f9c29facbeaf5730a
Alexey <algoritm211@gmail.com>
2022-03-08 20:35:20 +0200

    Repeat Counting sort

--- sortingAlgorithms/countingSort/countingSort.ts
+++ sortingAlgorithms/countingSort/countingSort.ts
@@ -1,24 +1,24 @@
 const countingSortTest = [1, 3, 3, 2, 9, 8, 4, 1, 8, 5, 3, 6]
 
 /**
- * Complexity
+ * Complexity n + r (r - biggest number in array)
  */
 const countingSort = (arr: Array<number>, min: number, max: number) => {
   const resultArr: Array<number> = [];
-  const subArray: Array<number> = [];
+  const helperArr: Array<number> = [];
 
-  for (let i = min; i <= max; i++) {
-    subArray[i] = 0;
+  for (let i = min; i < max; i++) {
+    helperArr[i] = 0;
   }
 
   for (let i = 0; i < arr.length; i++) {
-    subArray[arr[i]] += 1
+    helperArr[arr[i]] += 1
   }
 
   for (let i = min; i < max; i++) {
-    while (subArray[i] > 0) {
-      resultArr.push(i)
-      subArray[i] -= 1;
+    while (helperArr[i] > 0) {
+      resultArr.push(i);
+      helperArr[i] -= 1;
     }
   }
 
